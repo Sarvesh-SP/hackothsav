18a48e78953b076fdcb21eb42f00bbacfeb70dfd
Vinith69 <ramarock09@gmial.com>
2021-06-09 14:18:54 +0530

    theme button added

--- scripts/themeToggle.js
+++ scripts/themeToggle.js
@@ -1,12 +1,21 @@
 let themeButton = document.getElementById("theme-button");
 let navBar = document.querySelector("nav-bar");
+const sunMoonContainer = document.querySelector(".sun-moon-container");
 // let body = document.body;
 
 const changeTheme = () => {
-	// console.log(document.documentElement.getAttribute("theme"));
-	if (document.documentElement.getAttribute("theme") == "dark")
+	console.log(document.documentElement.getAttribute("theme"));
+	if (document.documentElement.getAttribute("theme") == "dark") {
 		document.documentElement.setAttribute("theme", "light");
-	else document.documentElement.setAttribute("theme", "dark");
+		body.classList.toggle("dark");
+	} else {
+		body.classList.toggle("dark");
+		document.documentElement.setAttribute("theme", "dark");
+	}
+	const currentRotation = parseInt(
+		getComputedStyle(sunMoonContainer).getPropertyValue("--rotation")
+	);
+	sunMoonContainer.style.setProperty("--rotation", currentRotation + 180);
 };
 
 themeButton.addEventListener("click", changeTheme);
